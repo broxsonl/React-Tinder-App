dffab3e4625bf716964529d93aef4234629c0d40
Lee Broxson <broxsonl@gmail.com>
2016-12-27 20:15:26 -0800

    Added gitignores and linter config files.

---
+++ src/reducers/reducer-books.js
@@ -0,0 +1,10 @@
+'use strict';
+
+export default () => {
+  return [
+    { title: 'You Don\'t Know JS.'},
+    { title: 'Name of the Wind'},
+    { title: 'Lord of the Rings'},
+    { title: 'Dune'},
+  ];
+};
